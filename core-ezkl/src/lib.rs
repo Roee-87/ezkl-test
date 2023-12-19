900dc093dc29d3b5b439a275dde3e2bf24d538c5
Roy Rotstein <royrotstein@Roys-MacBook-Pro-2.local>
2023-12-18 16:53:41 -0800

    added params import function

--- core-ezkl/src/lib.rs
+++ core-ezkl/src/lib.rs
@@ -1,8 +1,19 @@
 use halo2curves::ff::{FromUniformBytes, PrimeField, WithSmallOrderMulGroup};
 use halo2curves::serde::SerdeObject;
+use halo2curves::bn256::Bn256;
+use halo2_proofs::poly::commitment::{Params, CommitmentScheme};
+use halo2_proofs::poly::kzg::commitment::{KZGCommitmentScheme, ParamsKZG};
+use halo2_proofs::poly::commitment::ParamsProver;
+use halo2_proofs::plonk::{
+    verify_proof, Circuit, VerifyingKey,
+};
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
-use clap::ValueEnum;
+use clap::{Args, ValueEnum};
+use halo2curves::CurveAffine;
+use std::ops::Deref;
+
+
 //use snark_verifier::verifier::plonk::PlonkProtocol;
 use std::{
     fs,
@@ -48,13 +59,142 @@ pub struct Snark<F: PrimeField + SerdeObject>
     pub split: Option<ProofSplitCommit>,
 }
 
-impl<F: PrimeField + SerdeObject + Serialize + FromUniformBytes<64> + DeserializeOwned> Snark<F> {
+impl<F: PrimeField + SerdeObject + Serialize + FromUniformBytes<64> + DeserializeOwned> Snark<F>
+{
     pub fn load(
         proof_path: &str,
-    ) -> Self {
-        let mut f1 = File::open(proof_path).expect("Could not find path");
-        let mut json_file = String::new();
-        f1.read_to_string(&mut json_file).expect("Unable to read to string");
-        serde_json::from_str(&json_file).unwrap()
+    ) -> Self 
+    {
+        // let mut f1 = File::open(proof_path).expect("Could not find path");
+        // let mut json_file = String::new();
+        // f1.read_to_string(&mut json_file).expect("Unable to read to string");
+        serde_json::from_str(proof_path).unwrap()
     }
+}
+impl<Scalar:  SerdeObject + PrimeField + FromUniformBytes<64> + WithSmallOrderMulGroup<3> + Ord + Serialize + DeserializeOwned> Snark<Scalar> {
+    pub fn format_instances(&self)
+    {
+        let pi_inner = self
+        .instances
+        .iter()
+        .map(|e| e.deref())
+        .collect::<Vec<&[Scalar]>>();
+        let instances: &[&[&[Scalar]]] = &[&pi_inner];
+        println!("Instances: {:?}", &instances);
+    }
+}
+
+
+
+#[derive(Debug, Args, Deserialize, Serialize, Clone, Default, PartialEq, PartialOrd)]
+pub struct RunArgs {
+    #[serde(skip)]
+    pub tolerance: String,
+    #[serde(skip)]
+    pub input_scale: String,
+    #[serde(skip)]
+    pub param_scale: String,
+    #[serde(skip)]
+    pub scale_rebase_multiplier: u32,
+    /// The min and max elements in the lookup table input column
+    #[serde(skip)]
+    pub lookup_range: i128,
+    /// The log_2 number of rows
+    #[arg(short = 'K', long, default_value = "17")]
+    pub logrows: u32,
+    /// The log_2 number of rows
+    #[arg(short = 'N', long, default_value = "2")]
+    #[serde(skip)]
+    pub num_inner_cols: usize,
+    /// Hand-written parser for graph variables, eg. batch_size=1
+    #[serde(skip)]
+    pub variables: Vec<String>,
+    /// Flags whether inputs are public, private, hashed
+    #[arg(long, default_value = "private")]
+    #[serde(skip)]
+    pub input_visibility: String,
+    /// Flags whether outputs are public, private, hashed
+    #[arg(long, default_value = "public")]
+    #[serde(skip)]
+    pub output_visibility: String,
+    /// Flags whether params are public, private, hashed
+    #[arg(long, default_value = "private")]
+    #[serde(skip)]
+    pub param_visibility: String,
+}
+
+#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
+pub struct GraphSettings {
+    /// run args
+    pub run_args: RunArgs,
+    /// the potential number of rows used by the circuit
+    pub num_rows: usize,
+    /// total linear coordinate of assignments
+    pub total_assignments: usize,
+    /// total const size
+    pub total_const_size: usize,
+    /// the shape of public inputs to the model (in order of appearance)
+    #[serde(skip)]
+    pub model_instance_shapes: Vec<Vec<usize>>,
+    /// model output scales
+    #[serde(skip)]
+    pub model_output_scales: Vec<String>,
+    /// model input scales
+    #[serde(skip)]
+    pub model_input_scales: Vec<String>,
+    /// the of instance cells used by modules
+    #[serde(skip)]
+    pub module_sizes: String,
+    /// required_lookups
+    #[serde(skip)]
+    pub required_lookups: String,
+    /// check mode
+    #[serde(skip)]
+    pub check_mode: String,
+    /// ezkl version used
+    pub version: String,
+    /// num blinding factors
+    pub num_blinding_factors: Option<usize>,
+}
+
+fn get_log_rows(settings_path: &str) -> u32 {
+    let settings: GraphSettings = serde_json::from_str(&settings_path).unwrap();
+    settings.run_args.logrows
+}
+
+pub fn get_verifier_params(settings_path: &str, srs_path: &str) -> ParamsKZG<Bn256> {
+    // read in log_rows from teh settings struct
+    let logrows = get_log_rows(settings_path);
+
+    // read in the params binary file as bytes
+    let mut f = File::open(srs_path).expect("File not found");
+    let metadata = fs::metadata(srs_path).expect("unable to read metadata");
+    let mut buf: Vec<u8> = vec![0; metadata.len() as usize];
+    f.read(&mut buf).expect("Buffer overflow");
+
+    // deserialize the params and downsize if necessary
+    let mut params: ParamsKZG<Bn256> = Params::read::<_>(&mut &buf[..]).unwrap();
+    if logrows < params.k() {
+       params.downsize(logrows);
+    }
+    params
+    // // obtain the verifier params and serialize to bytes
+    // let v_params = params.verifier_params();
+    // let mut v_params_bytes: Vec<u8> = Vec::new();
+    // let _ = <ParamsKZG<_> as Params<_>>::write(&v_params, &mut v_params_bytes);
+    // v_params_bytes
+}
+pub fn get_verifier_key(vk_path: &str, params: ParamsKZG<Bn256>) -> VerifyingKey<Bn256>
+where 
+    C: Circuit<Scalar: Serialize + DeserializeOwned> 
+    {
+    //read in the path
+    let mut f = File::open(vk_path).expect("File not found");
+    let mut reader = BufReader::new(f);
+    VerifyingKey::Bn256::read::<_, C>(
+        &mut reader,
+        halo2_proofs::SerdeFormat::RawBytes,
+        params,
+    )
+    .expect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-use core_ezkl::Snark;
-use halo2curves::bn256::Fr;
+use core_ezkl::*;
+use halo2curves::bn256::{Bn256, Fr};
+use halo2curves::CurveAffine;
+use halo2_proofs::poly::commitment::{Params, CommitmentScheme};
+use halo2_proofs::poly::kzg::commitment::{KZGCommitmentScheme, ParamsKZG};
+
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+
 // use halo2curves::bn256::fr::Fr;
 // use halo2_proofs::poly::kzg::commitment::{KZGCommitmentScheme, ParamsKZG};
 
-
+const SETTINGS_JSON: &str = include_str!("../settings.json");
+const KZG_SRS: &str = "kzg.srs";
+const VK: &str = "test.vk";
+const PROOF: &str = include_str!("../proof.json");
 
 fn main() {
-    let ezkl_snark = Snark::<Fr>::load::<Fr>("proof.json");
-    let proof_bytes = &ezkl_snark.proof[..];
-    let instances = ezkl_snark.instances;
-    println!("Proof bytes is: {:?}", &proof);
+    let v_param = get_verifier_params(SETTINGS_JSON, KZG_SRS);
+    //println!("params k: {:?}", param_bytes);
+    let vk = get_verifier_key(VK);)  
+    let ezkl_snark = Snark::<Fr>::load(PROOF);
+    // let proof_bytes = &ezkl_snark.proof[..];
+    //let instances = ezkl_snark.instances;
+    //let _ = ezkl_snark.format_instances();
+    // println!("Proof bytes is: {:?}", &ezkl_snark.instances);
+
+
 }
 
 
